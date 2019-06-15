efdb0b2ba600a3df2736a19a1c69a2a7fe320b40
Robert Cowher <robert.cowher@grizzlysts.com>
2019-06-14 22:34:09 -0500

    speed settings

--- pibot_lib.py
+++ pibot_lib.py
@@ -7,8 +7,7 @@ mh = Raspi_MotorHAT(addr=0x6f)
 lm = mh.getMotor(1)
 rm = mh.getMotor(2)
 
-lm.setSpeed(150)
-rm.setSpeed(150)
+
 
 atexit.register(turn_off_motors)
 
@@ -18,20 +17,28 @@ def turn_off_motors():
     rm.run(Raspi_MotorHAT.RELEASE)
 
 def forward(time):
+    lm.setSpeed(150)
+    rm.setSpeed(150)
     lm.run(Raspi_MotorHAT.FORWARD)
     rm.run(Raspi_MotorHAT.FORWARD)
     time.sleep(time)
 
 def back(time):
+    lm.setSpeed(150)
+    rm.setSpeed(150)
     lm.run(Raspi_MotorHAT.BACKWARD)
     rm.run(Raspi_MotorHAT.BACKWARD)
     time.sleep(time)
 
 def turn_left():
+    lm.setSpeed(50)
+    rm.setSpeed(50)
     lm.run(Raspi_MotorHAT.FORWARD)
     rm.run(Raspi_MotorHAT.BACKWARD)
 
 def turn_right():
+    lm.setSpeed(50)
+    rm.setSpeed(50)
     lm.run(Raspi_MotorHAT.BACKWARD)
     rm.run(Raspi_MotorHAT.FORWARD)
 
