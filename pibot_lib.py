81f3979950df833ddbce57fd692f270abaec5650
Robert Cowher <robert.cowher@grizzlysts.com>
2019-06-14 22:31:45 -0500

    pibot lib

---
+++ pibot_lib.py
@@ -0,0 +1,41 @@
+import time
+import atexit
+from Raspi_MotorHAT import Raspi_MotorHAT
+
+mh = Raspi_MotorHAT(addr=0x6f)
+
+lm = mh.getMotor(1)
+rm = mh.getMotor(2)
+
+lm.setSpeed(150)
+rm.setSpeed(150)
+
+atexit.register(turn_off_motors)
+
+
+def turn_off_motors():
+    lm.run(Raspi_MotorHAT.RELEASE)
+    rm.run(Raspi_MotorHAT.RELEASE)
+
+def forward(time):
+    lm.run(Raspi_MotorHAT.FORWARD)
+    rm.run(Raspi_MotorHAT.FORWARD)
+    time.sleep(time)
+
+def back(time):
+    lm.run(Raspi_MotorHAT.BACKWARD)
+    rm.run(Raspi_MotorHAT.BACKWARD)
+    time.sleep(time)
+
+def turn_left()
+    lm.run(Raspi_MotorHAT.FORWARD)
+    rm.run(Raspi_MotorHAT.BACKWARD)
+
+def turn_right()
+    lm.run(Raspi_MotorHAT.BACKWARD)
+    rm.run(Raspi_MotorHAT.FORWARD)
+
+
+
+
+
