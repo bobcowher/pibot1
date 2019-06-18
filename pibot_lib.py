dd5ce4e05930ee543bfd2e8d9d1740f40ec37203
Robert Cowher <robert.cowher@grizzlysts.com>
2019-06-17 19:32:05 -0500

    Object oriented now

--- pibot_lib.py
+++ pibot_lib.py
@@ -1,51 +1,27 @@
+import robot
 from time import sleep
-import atexit
-from Raspi_MotorHAT import Raspi_MotorHAT
 
-mh = Raspi_MotorHAT(addr=0x6f)
+def straight(bot, seconds):
+    bot.set_left(80)
+    bot.set_right(80)
+    sleep(seconds)
 
-lm = mh.getMotor(1)
-rm = mh.getMotor(2)
+def turn_left(bot, seconds):
+    bot.set_left(20)
+    bot.set_right(80)
+    sleep(seconds)
 
+def turn_right(bot, seconds):
+    bot.set_left(80)
+    bot.set_right(20)
+    sleep(seconds)
 
-def turn_off_motors():
-    lm.run(Raspi_MotorHAT.RELEASE)
-    rm.run(Raspi_MotorHAT.RELEASE)
-
-def forward(sleep_time):
-    lm.setSpeed(150)
-    rm.setSpeed(150)
-    lm.run(Raspi_MotorHAT.FORWARD)
-    rm.run(Raspi_MotorHAT.FORWARD)
-    sleep(sleep_time)
-    turn_off_motors()
-
-def back(sleep_time):
-    lm.setSpeed(150)
-    rm.setSpeed(150)
-    lm.run(Raspi_MotorHAT.BACKWARD)
-    rm.run(Raspi_MotorHAT.BACKWARD)
-    sleep(sleep_time)
-    turn_off_motors()
-
-def right():
-    lm.setSpeed(50)
-    rm.setSpeed(50)
-    lm.run(Raspi_MotorHAT.FORWARD)
-    rm.run(Raspi_MotorHAT.BACKWARD)
-    sleep(0.25)
-    turn_off_motors()
-
-def left():
-    lm.setSpeed(50)
-    rm.setSpeed(50)
-    lm.run(Raspi_MotorHAT.BACKWARD)
-    rm.run(Raspi_MotorHAT.FORWARD)
-    sleep(0.25)
-    turn_off_motors()
+def spin_left(bot, seconds):
+    bot.set_left(-80)
+    bot.set_right(80)
+    sleep(seconds)
 
 
-atexit.register(turn_off_motors)
 
 
 
---
+++ test_path.py
@@ -0,0 +1,13 @@
+from pibot_lib import *
+from robot import Robot
+
+bot = robot.Robot()
+
+straight(bot, 1)
+turn_right(bot, 1)
+straight(bot, 1)
+turn_left(bot, 1)
+straight(bot, 1)
+turn_left(bot, 1)
+straight(bot, 1)
+spin_left(bot, 1)
