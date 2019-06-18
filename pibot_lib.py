3662edff2268fb8dbae9629fe462ba575b24139d
Robert Cowher <robert.cowher@grizzlysts.com>
2019-06-17 19:49:10 -0500

    Path corrections

--- pibot_lib.py
+++ pibot_lib.py
@@ -3,7 +3,9 @@ from time import sleep
 
 def straight(bot, seconds):
     bot.set_left(80)
-    bot.set_right(79)
+    bot.set_right(70)
+    sleep(0.5)
+    bot.set_right(80)
     sleep(seconds)
 
 def reverse(bot, seconds):
