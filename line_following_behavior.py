bdb543044833d996e57ae6797aabdf049a40ffd0
Robert Cowher <robert.cowher@grizzlysts.com>
2019-07-05 23:15:59 -0500

    unstuck

--- line_following_behavior.py
+++ line_following_behavior.py
@@ -23,10 +23,28 @@ class LineFollowerBehavior:
 
     
     def run(self):
+        
+
         self.robot.left_line_sensor.when_line = self.when_left_crosses_line
+        self.robot.left_line_sensor.when_line = right_line_sensor_blocked = True
+
         self.robot.left_line_sensor.when_no_line = self.when_left_off_line
+        self.robot.left_line_sensor.when_no_line = right_line_sensor_blocked = False
+
+
         self.robot.right_line_sensor.when_line = self.when_right_crosses_line
+        self.robot.right_line_sensor.when_line = left_line_sensor_blocked = True
+
         self.robot.right_line_sensor.when_no_line = self.when_right_off_line
+        self.robot.right_line_sensor.when_no_line = left_line_sensor_blocked = False
+
+        if(left_line_sensor_blocked and right_line_sensor_blocked):
+            self.robot.set_left(self.cornering)
+            self.robot.set_right(self.cornering)
+            sleep(0.5)
+            self.robot.set_right(self.forward_speed)
+            sleep(0.1)
+
         sleep(0.02)
         self.robot.set_left(self.forward_speed)
         self.robot.set_right(self.forward_speed)
