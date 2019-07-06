c2c326f95d1736e2960df8d4699e909487ac7c2f
Robert Cowher <robert.cowher@grizzlysts.com>
2019-07-05 20:46:18 -0500

    Stop at line script + updated robotsetup.sh for gpiozero

--- robotsetup.sh
+++ robotsetup.sh
@@ -1,3 +1,3 @@
 sudo apt-get update -y
-sudo apt-get install -y git python-pip python-smbus i2c-tools
+sudo apt-get install -y git python-pip python-smbus i2c-tools python-gpiozero
 pip install git+https://github.com/orionrobots/Raspi_MotorHAT
