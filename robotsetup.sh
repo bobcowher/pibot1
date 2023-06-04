d3702f9eed9be0b609d81c57031805905337618a
Robert Cowher <bobcowher@gmail.com>
2023-06-04 14:05:35 -0500

    Updated ultrasonic distance

--- robotsetup.sh
+++ robotsetup.sh
@@ -1,3 +1,5 @@
 sudo apt-get update -y
 sudo apt-get install -y git python-pip python-smbus i2c-tools python-gpiozero
+sudo apt-get install
 pip install git+https://github.com/orionrobots/Raspi_MotorHAT
+pip install smbus
